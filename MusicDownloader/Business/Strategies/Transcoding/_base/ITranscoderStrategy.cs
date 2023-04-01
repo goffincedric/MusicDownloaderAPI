cdf3fbf1f6e0ec16feccea863b1486ac6513ecc9
Cédric Goffin <goffin.cedric@hotmail.com>
2023-04-01 03:40:46 +0200

    refactor: Transcode using urls instead of streams to avoid pipe broken errors

--- MusicDownloader/Business/Strategies/Transcoding/_base/ITranscoderStrategy.cs
+++ MusicDownloader/Business/Strategies/Transcoding/_base/ITranscoderStrategy.cs
@@ -7,12 +7,12 @@ public interface ITranscoderStrategy
     /// <summary>
     /// Executes transcoding processes for supported a supported container.
     /// </summary>
-    /// <param name="audioStreamTask">Task that contains a stream of the audio to transcode</param>
+    /// <param name="audioUrl">String that contains a url to the audio to transcode</param>
     /// <param name="coverArtStreamTask">Task that contains an optional stream of the cover art to include</param>
     /// <param name="trackMetadataTask">Task that contains an object with metadata about the audio, which will be included in the transcoded audio</param>
     /// <returns>An object containing the transcoded audio stream, along with the filename and container</returns>
     internal Task<MusicStream> Execute(
-        Task<Stream> audioStreamTask,
+        string audioUrl,
         Task<Stream?> coverArtStreamTask,
         Task<TrackMetadata> trackMetadataTask
     );
